b762f46e62d05c7db7b405ca9131154ed0f3c532
Nguyễn Thành Long <73679885+atemkumado@users.noreply.github.com>
2025-09-04 01:04:41 +0700

    update image project

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,3 @@
-import logo from "./logo.png";
 import logo_no_text from "./logo_no_text.png";
 import logo_dark from "./logo_dark.svg";
 import cross_icon from "./cross_icon.svg";
@@ -8,21 +7,25 @@ import left_arrow from "./left_arrow.svg";
 import right_arrow from "./right_arrow.svg";
 import header_img from "./header_img.jpg";
 import brand_img from "./brand_img.png";
-import project_img_1 from "./project_img_1.jpg";
-import project_img_2 from "./project_img_2.jpg";
-import project_img_3 from "./project_img_3.jpg";
 import project_img_4 from "./project_img_4.jpg";
 import project_img_5 from "./project_img_5.jpg";
 import project_img_6 from "./project_img_6.jpg";
-import reasons from "./reasons.jpg";
+import reasons from "./reasons.png";
 import profile_img_1 from "./profile_img_1.png";
 import profile_img_2 from "./profile_img_2.png";
 import profile_img_3 from "./profile_img_3.png";
 
-import project1_before from "./ccq8_bed2.jpg";
-import project1_after from "./ccq8_kitchen.jpg";
-import project2_before from "./project2_before.jpg";
-import project2_after from "./project2_after.jpg";
+import project_img_1 from "./project1.png";
+import project1_before from "./project1_before.png";
+import project1_after from "./project1_after.png";
+
+import project_img_2 from "./project2.png";
+import project2_before from "./project2_before.png";
+import project2_after from "./project2_after.png";
+
+import project_img_3 from "./project3.png";
+import project3_before from "./project3_before.png";
+import project3_after from "./project3_after.png";
 
 import about_us from "./about_us.jpg";
 import founder from "./founder.png";
@@ -32,7 +35,6 @@ import icon_service_design from "./service_design.png";
 import icon_service_install from "./service_install.png";
 
 export const assets = {
-  logo,
   logo_no_text,
   logo_dark,
   cross_icon,
@@ -56,30 +58,30 @@ export const assets = {
 
 export const projectsData = [
   {
-    title: "Skyline Haven",
-    price: "$2,50,000",
-    location: "California",
+    title: "Căn hộ 2PN",
+    price: "Phòng ngủ",
+    location: "Tối ưu tiện nghi",
     image: project_img_1,
     before: project1_before,
     after: project1_after,
     desc: "Cải tạo căn hộ cũ thành không gian sống hiện đại, tối ưu ánh sáng và công năng.",
   },
   {
-    title: "Vista Verde",
-    price: "$2,50,000",
-    location: "San Francisco",
+    title: "Nhà phố",
+    price: "Phòng ngủ",
+    location: "Tối ưu tiện nghi",
     image: project_img_2,
     before: project2_before,
     after: project2_after,
     desc: "Biến đổi nhà phố truyền thống thành tổ ấm tiện nghi, sang trọng.",
   },
   {
-    title: "Serenity Suites",
-    price: "$2,50,000",
-    location: "Chicago",
+    title: "Nhà phố",
+    price: "Bếp",
+    location: "Cải tạo không gian",
     image: project_img_3,
-    before: project1_before,
-    after: project1_after,
+    before: project3_before,
+    after: project3_after,
     desc: "Cải tạo căn hộ cũ thành không gian sống hiện đại, tối ưu ánh sáng và công năng.",
   },
   {
@@ -141,22 +143,22 @@ export const testimonialsData = [
 export const SERVICES = [
   {
     title: "Lên kế hoạch & tư vấn",
-    desc: "Định hướng chi phí, vật liệu và tiến độ rõ ràng, giúp khách hàng chủ động trong quá trình cải tạo.",
+    desc: "Định hướng chi phí, vật liệu và<br />tiến độ rõ ràng, giúp khách hàng <br />chủ động trong quá trình cải tạo.",
     icon: icon_service_plan, // hoặc đường dẫn svg tương ứng bạn có
   },
   {
     title: "Cải tạo & xây dựng không gian",
-    desc: "Tối ưu diện tích, bố trí lại công năng và thay đổi tổng thể để ngôi nhà trở nên tiện ích và hiện đại hơn.",
+    desc: "Tối ưu diện tích, bố trí lại công năng <br /> và thay đổi tổng thể để ngôi nhà <br /> trở nên tiện ích và hiện đại hơn.",
     icon: icon_service_build,
   },
   {
     title: "Thiết kế nội thất",
-    desc: "Đưa ra giải pháp thiết kế hài hòa, phù hợp với nhu cầu và ngân sách của từng khách hàng.",
+    desc: "Đưa ra giải pháp thiết kế hài hòa,<br /> phù hợp với nhu cầu và ngân sách <br /> của từng khách hàng.",
     icon: icon_service_design,
   },
   {
     title: "Thi công & lắp đặt nội thất",
-    desc: "Đảm bảo chất lượng, tiến độ và sự chỉn chu trong từng chi tiết thi công.",
+    desc: "Đảm bảo chất lượng, tiến độ và <br />  sự chỉn chu trong từng chi tiết thi công.",
     icon: icon_service_install,
   },
 ];
